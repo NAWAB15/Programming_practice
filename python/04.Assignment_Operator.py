6357e13f9cb29a05955a879ead3b3a89a8cf3470
Nawab15 <128088611+NAWAB15@users.noreply.github.com>
2023-03-27 01:09:48 +0500

    Assignment operator in python

---
+++ python/04.Assignment_Operator.py
@@ -0,0 +1,18 @@
+#some important operator in python
+a=10.2
+b=5
+#1.Arithmetic operator in python
+# ADD
+print(a+b)
+#subtract
+print(a-b)
+#Multiply
+print(a*b)
+#Division
+print(a/b)
+#exponentional
+print(a**b)
+#floor devision
+print(a//b)
+#Modules
+print(a%b)
