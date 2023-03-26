6a5c5106c3eb88b3f395804cd161ddfe7bb7ff06
Nawab15 <128088611+NAWAB15@users.noreply.github.com>
2023-03-27 00:44:22 +0500

    two variable and display calculation

---
+++ python/05_Simple_calculator.py
@@ -0,0 +1,9 @@
+#create simple calculator just for two number and display them.
+a=4
+b=2
+print("sum:",a+b)
+print("subtraction:",a-b)
+print("Multipliction:",a+b)
+print("Division:",a/b)
+print("power:",a**b)
+print("remainder",a%b)
