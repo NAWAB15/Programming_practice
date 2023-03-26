9abb25353c5bf7e840af20da92663bdbb8cbad1b
Nawab15 <128088611+NAWAB15@users.noreply.github.com>
2023-03-27 00:37:42 +0500

    Convert one data type to another

---
+++ python/03_Type_casting.py
@@ -0,0 +1,19 @@
+
+#type casting is convert one data type to another data type.
+#1.int to float
+a=10
+b=5
+print(float(a)+float(b))
+# or
+c=a+b
+print("sum",float(c))
+
+#2.float to int
+a=10.2
+b=5.1
+print(int(a)+int(b))
+#or
+c=a+b
+print("sum",int (c))
+
+
